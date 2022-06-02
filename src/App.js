f358369c7af0f64580094016dabcf1f02bd4b6dd
Gulam Ansari <gulamansari57181@gmail.com>
2022-06-02 22:49:25 +0530

    HeroSection , CardSection and Footer Component implemented

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import React from "react";
 import Navbar from "./Navbar";
+import HeroSection from "./HeroSection";
+import AppSection from "./AppSection";
+import Card from "./Card";
+import Footer from "./Footer";
 
 function App() {
   return (
     <div>
       <Navbar></Navbar>
+      <HeroSection></HeroSection>
+      <AppSection></AppSection>
+      <Card></Card>
+      <Footer></Footer>
     </div>
   );
 }
---
+++ src/Footer.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <div style={{ marginTop: "30px" }}>
+      <section className="footer  bg-dark text-white">
+        <p className="lead container"> Contact us at Instagram </p>{" "}
+        <p clclassNameass="lead container"> You can react via email too </p>{" "}
+        <p clclassNameass="lead container"> NO COPYRIGHT </p>{" "}
+      </section>{" "}
+    </div>
+  );
+}
+
+export default Footer;
